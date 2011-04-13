7d2c3b1af318974cc1ca06eeff188183626a1d90
cmsbuild <cmsbuild@cern.ch>
2011-04-13 13:56:35 +0000

    Autogenerated python configuration files for CMSSW_4_2_1

--- FWCore/Integration/WhatsItESProducer_cfi.py
+++ FWCore/Integration/WhatsItESProducer_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 WhatsItESProducer = cms.ESProducer('WhatsItESProducer',
-  test = cms.untracked.bool(False)
+  test = cms.untracked.bool(False),
+  appendToDataLabel = cms.string('')
 )
--- HLTrigger/JetMET/hltHcalMETNoiseFilter_cfi.py
+++ HLTrigger/JetMET/hltHcalMETNoiseFilter_cfi.py
@@ -7,8 +7,8 @@ hltHcalMETNoiseFilter = cms.EDFilter('HLTHcalMETNoiseFilter',
   numRBXsToConsider = cms.int32(2),
   needEMFCoincidence = cms.bool(True),
   minRBXEnergy = cms.double(50),
-  minRatio = cms.double(0.65),
-  maxRatio = cms.double(0.98),
+  minRatio = cms.double(-999),
+  maxRatio = cms.double(999),
   minHPDHits = cms.int32(17),
   minRBXHits = cms.int32(999),
   minHPDNoOtherHits = cms.int32(10),
@@ -18,5 +18,38 @@ hltHcalMETNoiseFilter = cms.EDFilter('HLTHcalMETNoiseFilter',
   maxRBXEMF = cms.double(0.02),
   minRecHitE = cms.double(1.5),
   minLowHitE = cms.double(10),
-  minHighHitE = cms.double(25)
+  minHighHitE = cms.double(25),
+  TS4TS5EnergyThreshold = cms.double(50),
+  TS4TS5UpperThreshold = cms.vdouble(
+    70,
+    90,
+    100,
+    400,
+    4000
+  ),
+  TS4TS5UpperCut = cms.vdouble(
+    1,
+    0.8,
+    0.75,
+    0.72,
+    0.72
+  ),
+  TS4TS5LowerThreshold = cms.vdouble(
+    100,
+    120,
+    150,
+    200,
+    300,
+    400,
+    500
+  ),
+  TS4TS5LowerCut = cms.vdouble(
+    -1,
+    -0.7,
+    -0.4,
+    -0.2,
+    -0.08,
+    0,
+    0.1
+  )
 )
