fe986c771a14760eacfbbe98bb5202580d5af33f
cmsbuild <cmsbuild@cern.ch>
2011-06-22 12:08:41 +0000

    Autogenerated python configuration files for CMSSW_4_2_4_hltpatch1

---
+++ HLTrigger/Muon/hltMuonDimuonL3Filter_cfi.py
@@ -0,0 +1,26 @@
+import FWCore.ParameterSet.Config as cms
+
+hltMuonDimuonL3Filter = cms.EDFilter('HLTMuonDimuonL3Filter',
+  BeamSpotTag = cms.InputTag('hltOfflineBeamSpot'),
+  CandTag = cms.InputTag('hltL3MuonCandidates'),
+  PreviousCandTag = cms.InputTag(''),
+  FastAccept = cms.bool(False),
+  MaxEta = cms.double(2.5),
+  MinNhits = cms.int32(0),
+  MaxDr = cms.double(2),
+  MaxDz = cms.double(9999),
+  ChargeOpt = cms.int32(0),
+  MinPtPair = cms.double(0),
+  MinPtMax = cms.double(3),
+  MinPtMin = cms.double(3),
+  MinInvMass = cms.double(2.8),
+  MaxInvMass = cms.double(3.4),
+  MinAcop = cms.double(-1),
+  MaxAcop = cms.double(3.15),
+  MinPtBalance = cms.double(-1),
+  MaxPtBalance = cms.double(999999),
+  NSigmaPt = cms.double(0),
+  saveTags = cms.bool(False),
+  MaxDCAMuMu = cms.double(99999.9),
+  MaxRapidityPair = cms.double(999999)
+)
---
+++ HLTrigger/Muon/hltMuonL2PreFilter_cfi.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+hltMuonL2PreFilter = cms.EDFilter('HLTMuonL2PreFilter',
+  BeamSpotTag = cms.InputTag('hltOfflineBeamSpot'),
+  CandTag = cms.InputTag('hltL2MuonCandidates'),
+  PreviousCandTag = cms.InputTag(''),
+  SeedMapTag = cms.InputTag('hltL2Muons'),
+  MinN = cms.int32(1),
+  MaxEta = cms.double(2.5),
+  MinNhits = cms.int32(0),
+  MaxDr = cms.double(9999),
+  MaxDz = cms.double(9999),
+  MinPt = cms.double(0),
+  NSigmaPt = cms.double(0),
+  saveTags = cms.bool(False)
+)
