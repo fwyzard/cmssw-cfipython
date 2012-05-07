fb20b35780db4e6973ebd002add8ce981d9a5cf9
cmsbuild <cmsbuild@cern.ch>
2012-05-07 22:08:39 +0000

    Autogenerated python configuration files for CMSSW_5_3_0

--- HLTrigger/HLTfilters/hlt12HLTDoubletDZIN4reco20RecoChargedCandidateES1_E_cfi.py
+++ HLTrigger/HLTfilters/hlt12HLTDoubletDZIN4reco20RecoChargedCandidateES1_E_cfi.py
@@ -10,6 +10,5 @@ hlt12HLTDoubletDZIN4reco20RecoChargedCandidateES1_E = cms.EDFilter('HLT2MuonMuon
   triggerType2 = cms.int32(0),
   MinDR = cms.double(-1),
   MaxDZ = cms.double(0.2),
-  checkSC = cms.bool(False),
   MinN = cms.int32(1)
 )
