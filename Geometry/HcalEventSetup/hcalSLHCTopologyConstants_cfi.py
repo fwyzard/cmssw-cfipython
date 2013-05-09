6b13fd528feb0086d7956ccd8a9c5467cb8388f6
cmsbuild <cmsbuild@cern.ch>
2013-05-09 13:00:09 +0000

    Autogenerated python configuration files for CMSSW_6_2_0_pre6

--- Geometry/HcalEventSetup/hcalSLHCTopologyConstants_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-hcalSLHCTopologyConstants = cms.ESProducer('HcalTopologyIdealEP',
-  mode = cms.string('HcalTopologyMode::SLHC'),
-  maxDepthHB = cms.int32(7),
-  maxDepthHE = cms.int32(7),
-  appendToDataLabel = cms.string('')
-)
