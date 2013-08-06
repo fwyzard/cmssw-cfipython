54c13868e7d667cadecb52cd06c419cc87bb03e0
cmsbuild <cmsbuild@cern.ch>
2013-08-06 22:35:58 +0200

    Autogenerated python configuration files for CMSSW_6_2_0_p1_gcc481

--- Geometry/FCalGeometry/ShashlikGeometryESProducer_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ShashlikGeometryESProducer = cms.ESProducer('ShashlikGeometryESProducer',
-  appendToDataLabel = cms.string('')
-)
--- IOMC/EventVertexGenerators/HLLHCEvtVtxGeneratorFix_cfi.py
+++
@@ -1,3 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-HLLHCEvtVtxGeneratorFix = cms.EDProducer('HLLHCEvtVtxGeneratorFix')
