44ee44965407c84612ff6d9d4b0b91013db5cced
cmsbuild <cmsbuild@cern.ch>
2013-10-02 14:03:08 -0700

    Autogenerated python configuration files for CMSSW_7_0_0_pre5

--- HLTrigger/special/hltPixlMBFilt_cfi.py
+++ HLTrigger/special/hltPixlMBFilt_cfi.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-hltPixlMBFilt = cms.EDFilter('HLTPixlMBForAlignmentFilter',
+hltPixlMBFilt = cms.EDFilter('HLTPixlMBFilt',
   saveTags = cms.bool(False),
   pixlTag = cms.InputTag('hltPixelCands'),
-  MinPt = cms.double(5),
+  MinPt = cms.double(0),
   MinTrks = cms.uint32(2),
-  MinSep = cms.double(1),
-  MinIsol = cms.double(0.05)
+  MinSep = cms.double(1)
 )
