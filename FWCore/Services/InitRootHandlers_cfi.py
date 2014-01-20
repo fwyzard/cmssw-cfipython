905bf06c25ecf5b37edc20798f0358eeb4728893
cmsbuild <cmsbuild@cern.ch>
2014-01-20 02:45:37 -0800

    Autogenerated python configuration files for CMSSW_7_0_0_pre12

--- FWCore/Services/InitRootHandlers_cfi.py
+++ FWCore/Services/InitRootHandlers_cfi.py
@@ -4,5 +4,6 @@ InitRootHandlers = cms.Service('InitRootHandlers',
   UnloadRootSigHandler = cms.untracked.bool(False),
   ResetRootErrHandler = cms.untracked.bool(True),
   AutoLibraryLoader = cms.untracked.bool(True),
-  AbortOnSignal = cms.untracked.bool(True)
+  AbortOnSignal = cms.untracked.bool(True),
+  DebugLevel = cms.untracked.int32(0)
 )
