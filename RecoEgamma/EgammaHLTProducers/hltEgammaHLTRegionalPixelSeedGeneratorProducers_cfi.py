7feb4bf3bebd3e7ee777def2d1048ac582d77a24
cmsbuild <cmsbuild@cern.ch>
2014-03-06 15:22:25 +0100

    Autogenerated python configuration files for CMSSW_7_1_0_pre4

--- RecoEgamma/EgammaHLTProducers/hltEgammaHLTRegionalPixelSeedGeneratorProducers_cfi.py
+++ RecoEgamma/EgammaHLTProducers/hltEgammaHLTRegionalPixelSeedGeneratorProducers_cfi.py
@@ -14,7 +14,7 @@ hltEgammaHLTRegionalPixelSeedGeneratorProducers = cms.EDProducer('EgammaHLTRegio
   TTRHBuilder = cms.string('WithTrackAngle'),
   OrderedHitsFactoryPSet = cms.PSet(
     ComponentName = cms.string('StandardHitPairGenerator'),
-    SeedingLayers = cms.InputTag('hltESPPixelLayerPairs'),
+    SeedingLayers = cms.InputTag('PixelLayerPairs'),
     maxElement = cms.uint32(0)
   )
 )
