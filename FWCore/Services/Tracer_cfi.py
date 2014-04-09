d76260bec192a9eb1a179833cfcebf770591e852
cmsbuild <cmsbuild@cern.ch>
2014-04-10 01:08:18 +0200

    Autogenerated python configuration files for CMSSW_7_1_0_pre6

--- FWCore/Services/Tracer_cfi.py
+++ FWCore/Services/Tracer_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 Tracer = cms.Service('Tracer',
   indention = cms.untracked.string('++'),
   dumpContextForLabels = cms.untracked.vstring(),
-  dumpNonModuleContext = cms.untracked.bool(False)
+  dumpNonModuleContext = cms.untracked.bool(False),
+  printTimestamps = cms.untracked.bool(False)
 )
