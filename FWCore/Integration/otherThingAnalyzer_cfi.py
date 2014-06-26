1646f1671654298e55e3f5f3f77187f3df75519b
cmsbuild <cmsbuild@cern.ch>
2014-06-26 14:28:40 +0200

    Autogenerated python configuration files for CMSSW_7_1_1

--- FWCore/Integration/otherThingAnalyzer_cfi.py
+++ FWCore/Integration/otherThingAnalyzer_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
-otherThingAnalyzer = cms.EDAnalyzer('OtherThingAnalyzer',
-  thingWasDropped = cms.untracked.bool(False),
-  other = cms.untracked.InputTag('OtherThing', 'testUserTag')
+otherThingAnalyzer = cms.EDProducer('ThingWithMergeProducer',
+  changeIsEqualValue = cms.untracked.bool(False),
+  labelsToGet = cms.untracked.vstring(),
+  noPut = cms.untracked.bool(False)
 )
