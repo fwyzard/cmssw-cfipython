c8f0df3264ef0880627de1cd91a30d772d4578b9
cmsbuild <cmsbuild@cern.ch>
2014-10-30 15:04:50 +0100

    Autogenerated python configuration files for CMSSW_7_2_1_patch1

--- HLTrigger/JSONMonitoring/triggerJSONMonitoring_cfi.py
+++ HLTrigger/JSONMonitoring/triggerJSONMonitoring_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 triggerJSONMonitoring = cms.EDAnalyzer('TriggerJSONMonitoring',
-  triggerResults = cms.InputTag('TriggerResults', '', 'HLT')
+  triggerResults = cms.InputTag('TriggerResults', '', 'HLT'),
+  L1GtObjectMapTag = cms.InputTag('hltL1GtObjectMap')
 )
