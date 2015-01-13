d0878225d200ce6c24378e88c8944125467e27e5
cmsbuild <cmsbuild@cern.ch>
2015-01-13 17:13:10 +0100

    Autogenerated python configuration files for CMSSW_7_4_0_pre3_ROOT6

--- HLTriggerOffline/SUSYBSM/hltJetSortedVBFFilterRecoCaloJet_cfi.py
+++ HLTriggerOffline/SUSYBSM/hltJetSortedVBFFilterRecoCaloJet_cfi.py
@@ -12,5 +12,6 @@ hltJetSortedVBFFilterRecoCaloJet = cms.EDFilter('HLTCaloJetSortedVBFFilter',
   Ptsumbb = cms.double(0),
   Etaq1Etaq2 = cms.double(40),
   value = cms.string('second'),
-  triggerType = cms.int32(85)
+  triggerType = cms.int32(85),
+  njets = cms.int32(4)
 )
--- HLTriggerOffline/SUSYBSM/hltJetSortedVBFFilterRecoPFJet_cfi.py
+++ HLTriggerOffline/SUSYBSM/hltJetSortedVBFFilterRecoPFJet_cfi.py
@@ -12,5 +12,6 @@ hltJetSortedVBFFilterRecoPFJet = cms.EDFilter('HLTPFJetSortedVBFFilter',
   Ptsumbb = cms.double(0),
   Etaq1Etaq2 = cms.double(40),
   value = cms.string('second'),
-  triggerType = cms.int32(85)
+  triggerType = cms.int32(85),
+  njets = cms.int32(4)
 )
