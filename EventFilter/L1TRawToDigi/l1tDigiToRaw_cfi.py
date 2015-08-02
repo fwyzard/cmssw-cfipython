743598fc050ec623ddd284e534ba441fa5de6ca6
cmsbuild <cmsbuild@cern.ch>
2015-08-02 21:36:42 +0200

    Autogenerated python configuration files for CMSSW_7_5_1

---
+++ EventFilter/L1TRawToDigi/l1tDigiToRaw_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+l1tDigiToRaw = cms.EDProducer('L1TDigiToRaw',
+  FWId = cms.uint32(4294967295),
+  eventType = cms.untracked.int32(1),
+  lenSlinkHeader = cms.untracked.int32(8),
+  lenSlinkTrailer = cms.untracked.int32(8)
+)
