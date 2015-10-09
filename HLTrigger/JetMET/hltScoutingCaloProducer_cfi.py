363206a47f492f0d6039e4e26b84a79a1ca7d881
cmsbuild <cmsbuild@cern.ch>
2015-10-09 14:50:33 +0200

    Autogenerated python configuration files for CMSSW_7_6_0_pre7

--- HLTrigger/JetMET/hltScoutingCaloProducer_cfi.py
+++ HLTrigger/JetMET/hltScoutingCaloProducer_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 hltScoutingCaloProducer = cms.EDProducer('HLTScoutingCaloProducer',
   caloJetCollection = cms.InputTag('hltAK4CaloJets'),
   vertexCollection = cms.InputTag('hltPixelVertices'),
-  metCollection = cms.InputTag('hltMetCleanUsingJetID'),
+  metCollection = cms.InputTag('hltMet'),
   rho = cms.InputTag('hltFixedGridRhoFastjetAllCalo'),
   caloJetPtCut = cms.double(20),
   caloJetEtaCut = cms.double(3),
--- RecoLocalCalo/HcalRecProducers/hltHbhereco_cfi.py
+++ RecoLocalCalo/HcalRecProducers/hltHbhereco_cfi.py
@@ -5,15 +5,15 @@ hltHbhereco = cms.EDProducer('HcalHitReconstructor',
   pedestalUpperLimit = cms.double(2.7),
   timeSlewParsType = cms.int32(3),
   timeSlewPars = cms.vdouble(
-    15.5,
-    -3.2,
-    32,
-    15.5,
-    -3.2,
-    32,
-    15.5,
-    -3.2,
-    32
+    12.2999,
+    -2.19142,
+    0,
+    12.2999,
+    -2.19142,
+    0,
+    12.2999,
+    -2.19142,
+    0
   ),
   respCorrM3 = cms.double(0.95)
 )
