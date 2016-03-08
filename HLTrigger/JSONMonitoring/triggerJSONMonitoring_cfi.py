e20576e2c4cca2683f82c2c5e151a61db38a3e74
cmsbuild <cmsbuild@cern.ch>
2016-03-08 17:45:21 +0100

    Autogenerated python configuration files for CMSSW_8_0_2

--- HLTrigger/JSONMonitoring/triggerJSONMonitoring_cfi.py
+++ HLTrigger/JSONMonitoring/triggerJSONMonitoring_cfi.py
@@ -2,5 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 triggerJSONMonitoring = cms.EDAnalyzer('TriggerJSONMonitoring',
   triggerResults = cms.InputTag('TriggerResults', '', 'HLT'),
-  L1Results = cms.InputTag('hltGtDigis')
+  L1Results = cms.InputTag('hltGtStage2Digis')
 )
