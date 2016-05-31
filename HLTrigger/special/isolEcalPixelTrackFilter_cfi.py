05cf6c4601c35288fe265b8497d84bc4780b25cb
cmsbuild <cmsbuild@cern.ch>
2016-05-31 18:09:21 +0200

    Autogenerated python configuration files for CMSSW_8_0_10

--- HLTrigger/special/isolEcalPixelTrackFilter_cfi.py
+++ HLTrigger/special/isolEcalPixelTrackFilter_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 isolEcalPixelTrackFilter = cms.EDFilter('HLTEcalPixelIsolTrackFilter',
   saveTags = cms.bool(True),
-  candTag = cms.InputTag('isolEcalPixelTrackProdHB'),
+  candTag = cms.InputTag('hltIsolEcalPixelTrackProd'),
   MaxEnergyIn = cms.double(1.2),
   MaxEnergyOut = cms.double(1.2),
   NMaxTrackCandidates = cms.int32(10),
