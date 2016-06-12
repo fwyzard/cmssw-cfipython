d424614d322b777d222b772e6d838f6792a24bc6
cmsbuild <cmsbuild@cern.ch>
2016-06-12 15:34:53 +0200

    Autogenerated python configuration files for CMSSW_8_1_0_pre7

--- Alignment/MillePedeAlignmentAlgorithm/millePedeFileExtractor_cfi.py
+++ Alignment/MillePedeAlignmentAlgorithm/millePedeFileExtractor_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 millePedeFileExtractor = cms.EDAnalyzer('MillePedeFileExtractor',
   fileDir = cms.string(''),
   outputBinaryFile = cms.string('milleBinary%04d.dat'),
-  fileBlobInputTag = cms.InputTag('millePedeFileConverter')
+  fileBlobInputTag = cms.InputTag('millePedeFileConverter'),
+  maxNumberOfBinaries = cms.int32(1000)
 )
--- SimTracker/TrackerHitAssociation/tpClusterProducer_cfi.py
+++ SimTracker/TrackerHitAssociation/tpClusterProducer_cfi.py
@@ -4,7 +4,9 @@ tpClusterProducer = cms.EDProducer('ClusterTPAssociationProducer',
   simTrackSrc = cms.InputTag('g4SimHits'),
   pixelSimLinkSrc = cms.InputTag('simSiPixelDigis'),
   stripSimLinkSrc = cms.InputTag('simSiStripDigis'),
+  phase2OTSimLinkSrc = cms.InputTag('simPh2OTDigis'),
   pixelClusterSrc = cms.InputTag('siPixelClusters'),
   stripClusterSrc = cms.InputTag('siStripClusters'),
+  phase2OTClusterSrc = cms.InputTag('siPhase2Clusters'),
   trackingParticleSrc = cms.InputTag('mix', 'MergedTrackTruth')
 )
