042b48a43257eb8366c719ce2a5335d0fdcdeb41
cmsbuild <cmsbuild@cern.ch>
2016-08-19 10:49:15 +0200

    Autogenerated python configuration files for CMSSW_8_0_18

--- RecoEgamma/EgammaHLTProducers/hltEcalPFClusterIsolationProducerRecoRecoChargedCandidate_cfi.py
+++ RecoEgamma/EgammaHLTProducers/hltEcalPFClusterIsolationProducerRecoRecoChargedCandidate_cfi.py
@@ -7,19 +7,13 @@ hltEcalPFClusterIsolationProducerRecoRecoChargedCandidate = cms.EDProducer('Muon
   doRhoCorrection = cms.bool(False),
   rhoMax = cms.double(99999999),
   rhoScale = cms.double(1),
+  effectiveAreaBarrel = cms.double(0.101),
+  effectiveAreaEndcap = cms.double(0.046),
   drMax = cms.double(0.3),
   drVetoBarrel = cms.double(0),
   drVetoEndcap = cms.double(0),
   etaStripBarrel = cms.double(0),
   etaStripEndcap = cms.double(0),
   energyBarrel = cms.double(0),
-  energyEndcap = cms.double(0),
-  effectiveAreas = cms.vdouble(
-    0.29,
-    0.21
-  ),
-  absEtaLowEdges = cms.vdouble(
-    0,
-    1.479
-  )
+  energyEndcap = cms.double(0)
 )
