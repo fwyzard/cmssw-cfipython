57b0a20ff35da16146eb443fb4ca75390776ffb3
cmsbuild <cmsbuild@cern.ch>
2017-02-26 13:26:13 +0100

    Autogenerated python configuration files for CMSSW_9_0_0_pre5

--- Geometry/HcalEventSetup/hcalTopologyIdeal_cfi.py
+++ Geometry/HcalEventSetup/hcalTopologyIdeal_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 hcalTopologyIdeal = cms.ESProducer('HcalTopologyIdealEP',
   Exclude = cms.untracked.string(''),
+  MergePosition = cms.untracked.bool(True),
   appendToDataLabel = cms.string('')
 )
