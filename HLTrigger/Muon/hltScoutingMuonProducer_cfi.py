6d57636c315230451f5873156df817e27f6d39e8
cmsbuild <cmsbuild@cern.ch>
2017-03-02 20:43:44 +0100

    Autogenerated python configuration files for CMSSW_9_0_0_pre6

--- HLTrigger/Muon/hltScoutingMuonProducer_cfi.py
+++ HLTrigger/Muon/hltScoutingMuonProducer_cfi.py
@@ -6,6 +6,8 @@ hltScoutingMuonProducer = cms.EDProducer('HLTScoutingMuonProducer',
   EcalPFClusterIsoMap = cms.InputTag('hltMuonEcalPFClusterIsoForMuons'),
   HcalPFClusterIsoMap = cms.InputTag('hltMuonHcalPFClusterIsoForMuons'),
   TrackIsoMap = cms.InputTag('hltMuonTkRelIsolationCut0p09Map', 'combinedRelativeIsoDeposits'),
+  displacedvertexCollection = cms.InputTag('hltDisplacedmumuVtxProducerDoubleMu3NoVtx'),
   muonPtCut = cms.double(4),
-  muonEtaCut = cms.double(2.4)
+  muonEtaCut = cms.double(2.4),
+  minVtxProbCut = cms.double(0.001)
 )
